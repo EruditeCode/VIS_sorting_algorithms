18f0b922a10bde48b6b85bb03b8781ca50a94a03
EruditeCode <erudite.home@gmail.com>
2022-10-27 21:35:21 +0100

    Updated the main function - see description.
    The main function was reorganised to make use of the Sort class status boolean which indicated if the object array was sorted or not. A reset button was added enabling the user to start the sort process again and set a new sort type. The colours of the interface were also improved and button positions altered. Overall, the main function had some initial refactoring to help tidy the code.

--- main.py
+++ main.py
@@ -20,95 +20,105 @@ def main():
 	# Initialise variables.
 	WINDOW_HEIGHT = 400
 	WINDOW_WIDTH = 800
-	sort_choice = "bubble"
-	number_elements = 100
-	range_min = 1
-	range_max = 50
-	play = False
-	sort = False
-	array = [randint(range_min, range_max + 1) for i in range(number_elements)]
-
 	screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 	clock = pygame.time.Clock()
 
+	# Background and surfaces for interface.
 	bg = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
 	bg.fill((20, 20, 20))
 	menu = pygame.Surface((180, WINDOW_HEIGHT))
-	menu.fill((150, 150, 150))
+	menu.fill((20, 20, 20))
 	disp = pygame.image.load('earth.jpg').convert_alpha()
 
 	# Buttons for sort choice.
 	bubble = pygame.Surface((30, 30))
 	quick = pygame.Surface((30, 30))
 	heap = pygame.Surface((30, 30))
+	# Buttons for play/pause and for reset.
+	play_btn = pygame.Surface((70, 30))
+	reset_btn = pygame.Surface((70, 30))
+	reset_btn.fill((60, 60, 60))
 
-	# Button for play.
-	play_btn = pygame.Surface((60, 30))
-	play_btn.fill("white")
+	# Initialising a sort object.
+	elements, range_min, range_max = 100, 1, 100
+	array = [randint(range_min, range_max + 1) for i in range(elements)]
+	choice = "bubble"
+	sort_obj = Sort(array, choice)
+	play = False
+	
 
-
+	# Initialising a counter to iterate over any frames.
 	count = 0
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				exit()
+
+			# Setting up user input features.
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				mouse_pos = pygame.mouse.get_pos()
 				if event.button == 1 and play == False:
 					# Check each sort button.
-					if in_rect(mouse_pos, 30, 60, 100, 130):
-						sort_choice = "bubble"
-					if in_rect(mouse_pos, 30, 60, 140, 170):
-						sort_choice = "quick"
-					if in_rect(mouse_pos, 30, 60, 180, 210):
-						sort_choice = "heap"
+					if in_rect(mouse_pos, 30, 60, 80, 110):
+						choice = "bubble"
+					if in_rect(mouse_pos, 30, 60, 120, 150):
+						choice = "quick"
+					if in_rect(mouse_pos, 30, 60, 160, 190):
+						choice = "heap"
 					if in_rect(mouse_pos, 30, 90, 320, 350):
+						# Check the play button.
 						play = True
-				if event.button == 1 and play == True:
-					if in_rect(mouse_pos, 30, 90, 320, 350):
-						array = [randint(range_min, range_max + 1) for i in range(number_elements)]
+					if in_rect(mouse_pos, 30, 90, 280, 310):
+						# Check the reset button.
+						array = [randint(range_min, range_max + 1) for i in range(elements)]
 						count = 0
-						sort = False
-						sort_obj = Sort(array, sort_choice)
+						sort_obj = Sort(array, choice)
+
+				elif event.button == 1 and play == True:
+					if in_rect(mouse_pos, 30, 90, 320, 350):
+						# Check the pause button.
+						play = False
 
 		# Displaying the background surface.
 		screen.blit(bg, (0, 0))
 		screen.blit(menu, (0, 0))
 		screen.blit(disp, (180, 0))
 
-		if sort_choice == "bubble":
-			bubble.fill((40, 40, 40))
-			quick.fill((200, 200, 200))
-			heap.fill((200, 200, 200))
-		elif sort_choice == "quick":
-			bubble.fill((200, 200, 200))
-			quick.fill((40, 40, 40))
-			heap.fill((200, 200, 200))
-		elif sort_choice == "heap":
-			bubble.fill((200, 200, 200))
-			quick.fill((200, 200, 200))
-			heap.fill((40, 40, 40))
+		# Selecting color and displaying the sort buttons.
+		bubble.fill((60, 60, 60)) if choice == "bubble" else bubble.fill((255, 255, 255))
+		quick.fill((60, 60, 60)) if choice == "quick" else quick.fill((255, 255, 255))
+		heap.fill((60, 60, 60)) if choice == "heap" else heap.fill((255, 255, 255))
+		screen.blit(bubble, (30, 80))
+		screen.blit(quick, (30, 120))
+		screen.blit(heap, (30, 160))
 
-		screen.blit(bubble, (30, 100))
-		screen.blit(quick, (30, 140))
-		screen.blit(heap, (30, 180))
+		# Selecting color and displaying the play/pause button.
+		play_btn.fill((162, 10, 10)) if play == True else play_btn.fill((0, 112, 61))
+		screen.blit(play_btn, (55, 350))
+		# Only show reset if the sort animation is not occuring.
+		if play == False:
+			screen.blit(reset_btn, (55, 310))
 
-		screen.blit(play_btn, (30, 320))
-
+		x = (WINDOW_WIDTH - 182) / len(array)
+		MAX_NUM = max(array)
 		if play == True:
-			if sort == True:
-				x = (WINDOW_WIDTH - 182) / len(array)
-				MAX_NUM = max(array)
+			if sort_obj.status == True:
 				for index, element in enumerate(sort_obj.frames[count]):
 					ob_height = (element / MAX_NUM) * (WINDOW_HEIGHT - 50)
-					pygame.draw.rect(screen, 'White', pygame.Rect(182 + (index * x), WINDOW_HEIGHT - ob_height, x - 2, ob_height))
+					pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(182 + (index * x), WINDOW_HEIGHT - ob_height, x - 2, ob_height))
 				if count < len(sort_obj.frames) - 1:
 					count += 1
+				else:
+					play = False
 			else:
-				sort_obj = Sort(array, sort_choice)
+				sort_obj = Sort(array, choice)
 				sort_obj.run_sort()
-				sort = True
+		if play == False:
+			for index, element in enumerate(sort_obj.frames[count]):
+				ob_height = (element / MAX_NUM) * (WINDOW_HEIGHT - 50)
+				pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(182 + (index * x), WINDOW_HEIGHT - ob_height, x - 2, ob_height))
+
 
 		pygame.display.update()
 		clock.tick(30)
