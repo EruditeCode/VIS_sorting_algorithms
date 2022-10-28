9187bd1eef227de0a2e75b1da4bb9e9a39551ba6
EruditeCode <erudite.home@gmail.com>
2022-10-28 20:54:58 +0100

    Added a swap counter on the interface and removed some variables.

--- main.py
+++ main.py
@@ -17,30 +17,36 @@ from sys import exit
 def main():
 	pygame.init()
 
-	# Initialise variables.
-	WINDOW_HEIGHT = 400
-	WINDOW_WIDTH = 800
+	# Initialise settings variables.
+	WINDOW_WIDTH, WINDOW_HEIGHT = 800, 400
+	play = False
+	mouse_sticky = False
+
+	# Initial user settings.
+	elements = 100
+	choice = "bubble"
+
+	# Initialise pygame settings
 	screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 	pygame.display.set_caption("Visual Sorts")
-	clock = pygame.time.Clock()
 	title_font = pygame.font.Font('LCD-U___.ttf', 28)
 	txt_font = pygame.font.Font('LCD-U___.ttf', 18)
+	clock = pygame.time.Clock()
 
-	# Background and surfaces for interface.
-	bg = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
-	bg.fill((20, 20, 20))
+	# Creating the background surfaces for the interface.
 	menu = pygame.Surface((180, WINDOW_HEIGHT))
 	menu.fill((20, 20, 20))
 	disp = pygame.image.load('earth.jpg').convert_alpha()
 
-	# Buttons for sort choice.
+	# Creating the buttons for the sort choices.
 	bubble = pygame.Surface((30, 30))
 	quick = pygame.Surface((30, 30))
 	heap = pygame.Surface((30, 30))
-	# Buttons for play/pause and for reset.
-	play_btn = pygame.Surface((70, 30))
+
+	# Creating the buttons for play/pause and reset.
 	reset_btn = pygame.Surface((70, 30))
 	reset_btn.fill((60, 60, 60))
+	play_btn = pygame.Surface((70, 30))
 
 	# Creating the text objects.
 	title = title_font.render('Visual Sorts', True, (255, 255, 255))
@@ -51,25 +57,19 @@ def main():
 	quickRect = quick_txt.get_rect(topleft=(63,126))
 	heap_txt = txt_font.render('Heap Sort', True, (255, 255, 255))
 	heapRect = heap_txt.get_rect(topleft=(63,166))
+	reset_txt = txt_font.render('Reset', True, (255, 255, 255))
+	resetRect = reset_txt.get_rect(topleft=(67,317))
 	play_txt = txt_font.render('Play', True, (255, 255, 255))
 	playRect = play_txt.get_rect(topleft=(70,356))
 	pause_txt = txt_font.render('Pause', True, (255, 255, 255))
 	pauseRect = pause_txt.get_rect(topleft=(65,356))
-	reset_txt = txt_font.render('Reset', True, (255, 255, 255))
-	resetRect = reset_txt.get_rect(topleft=(67,317))
+	swaps_txt = txt_font.render('Swaps:', True, (255, 255, 255))
+	swapsRect = swaps_txt.get_rect(topleft=(680,10))
 
 	# Initialising a sort object.
-	elements, range_min, range_max = 100, 1, 100
-	array = [randint(range_min, range_max + 1) for i in range(elements)]
-	choice = "bubble"
+	array = [randint(1, 101) for i in range(elements)]
 	sort_obj = Sort(array, choice)
-	play = False
-	circle_pos_x = 42 + (elements - 10) // 2
-	circle_pos = (circle_pos_x, 266)
-	mouse_sticky = False
-	
 
-	# Initialising a counter to iterate over any frames.
 	count = 0
 	while True:
 		for event in pygame.event.get():
@@ -88,27 +88,29 @@ def main():
 						choice = "quick"
 					if in_rect(mouse_pos, 25, 55, 160, 190):
 						choice = "heap"
+
+					# Check the number of elements slider.
 					if in_circle(mouse_pos, circle_pos, 7):
 						mouse_sticky = True
+
+					# Check the play and reset buttons.
 					if in_rect(mouse_pos, 55, 125, 350, 380):
-						# Check the play button.
 						play = True
 					if in_rect(mouse_pos, 55, 125, 310, 340):
-						# Check the reset button.
-						array = [randint(range_min, range_max + 1) for i in range(elements)]
+						array = [randint(1, 101) for i in range(elements)]
 						count = 0
 						sort_obj = Sort(array, choice)
 
 				elif event.button == 1 and play == True:
+					# Check the pause button.
 					if in_rect(mouse_pos, 55, 125, 350, 380):
-						# Check the pause button.
 						play = False
 
 			if event.type == pygame.MOUSEBUTTONUP:
 				if event.button == 1:
 					mouse_sticky = False
 
-		42 + (elements - 10) // 2
+
 		if mouse_sticky == True:
 			mouse_pos = pygame.mouse.get_pos()
 			if mouse_pos[0] > 137:
@@ -119,12 +121,11 @@ def main():
 				circle_pos = (mouse_pos[0], 266)
 			elements = ((circle_pos[0] - 42) * 2) + 10
 
-		# Displaying the background surface.
-		screen.blit(bg, (0, 0))
+		# Displaying the background menu and display surfaces.
 		screen.blit(menu, (0, 0))
 		screen.blit(disp, (180, 0))
 
-		# Selecting color and displaying the sort buttons.
+		# Displaying the sort buttons and setting their colors.
 		bubble.fill((0, 86, 62)) if choice == "bubble" else bubble.fill((255, 255, 255))
 		quick.fill((0, 86, 62)) if choice == "quick" else quick.fill((255, 255, 255))
 		heap.fill((0, 86, 62)) if choice == "heap" else heap.fill((255, 255, 255))
@@ -132,14 +133,15 @@ def main():
 		screen.blit(quick, (25, 120))
 		screen.blit(heap, (25, 160))
 
-		# Selecting color and displaying the play/pause button.
-		play_btn.fill((255, 127, 1)) if play == True else play_btn.fill((0, 86, 62))
-		screen.blit(play_btn, (55, 350))
-		# Only show reset if the sort animation is not occuring.
+		# Displaying the reset button if the animation is paused/finished.
 		if play == False:
 			screen.blit(reset_btn, (55, 310))
 
-		# Creating the slider widget.
+		# Displaying the play/pause button and setting their colors.
+		play_btn.fill((255, 127, 1)) if play == True else play_btn.fill((0, 86, 62))
+		screen.blit(play_btn, (55, 350))
+
+		# Displaying the slider widget.
 		circle_pos_x = 42 + (elements - 10) // 2
 		circle_pos = (circle_pos_x, 266)
 		pygame.draw.line(screen, (255, 255, 255), (42,265), (137,265), 4)
@@ -164,26 +166,28 @@ def main():
 				ob_height = (element / MAX_NUM) * (WINDOW_HEIGHT - 50)
 				pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(182 + (index * x), WINDOW_HEIGHT - ob_height, x - 2, ob_height))
 
-		# Create the dynamic text for number of elements.
-		n_elements = f"Elements: {elements}"
-		elements_txt = txt_font.render(n_elements, True, (255, 255, 255))
+		# Create the dynamic text for number of elements and swaps.
+		elements_txt = txt_font.render(f"Elements: {elements}", True, (255, 255, 255))
 		elementsRect = elements_txt.get_rect(topleft=(35,225))
-		screen.blit(elements_txt, elementsRect)
+		swaps_num = txt_font.render(f'{count:0>5}', True, (255, 255, 255))
+		swapsnumRect = swaps_num.get_rect(topleft=(737,10))
 
 		# Displaying all the text.
 		screen.blit(title, titleRect)
 		screen.blit(bubble_txt, bubbleRect)
 		screen.blit(quick_txt, quickRect)
 		screen.blit(heap_txt, heapRect)
+		screen.blit(elements_txt, elementsRect)
 		if play == False:
-			screen.blit(play_txt, playRect)
 			screen.blit(reset_txt, resetRect)
+			screen.blit(play_txt, playRect)
 		if play == True:
 			screen.blit(pause_txt, pauseRect)
-
+		screen.blit(swaps_txt, swapsRect)
+		screen.blit(swaps_num, swapsnumRect)
 
 		pygame.display.update()
-		clock.tick(30)
+		clock.tick(15)
 
 def in_rect(position, x_min, x_max, y_min, y_max):
 	if (x_min < position[0] < x_max) and (y_min < position[1] < y_max):
