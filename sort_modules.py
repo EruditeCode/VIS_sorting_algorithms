c892f1234124a1d442c720fb76867e089dc3f9f1
EruditeCode <erudite.home@gmail.com>
2022-11-01 17:17:55 +0000

    Updated the quick sort function to log a frame for each swap.

--- sort_modules.py
+++ sort_modules.py
@@ -1,3 +1,5 @@
+# Sort class to support the Visual Sorts program.
+
 from random import randint
 
 class Sort:
@@ -30,24 +32,27 @@ class Sort:
 	def quick_sort_frames(self, array):
 		self.quick_sort(array)
 		for pivot in self.item_log:
-			low, same, high = [], [], []
-			for value in self.frames[-1]:
-				if value < pivot:
-					low.append(value)
-				elif value == pivot:
-					same.append(value)
-				elif value > pivot:
-					high.append(value)
-			self.frames.append(low + same + high)
-
+			framing = True
+			while framing:
+				framing = False
+				placeholder = self.frames[-1].copy()
+				pivot_index = placeholder.index(pivot)
+				for index, value in enumerate(self.frames[-1]):
+					if ((value < pivot and index > pivot_index) or (value > pivot and index < pivot_index) or
+						((value == pivot and index != pivot_index) and (placeholder[index-1] != pivot))):
+						# Need to create a new frame.
+						placeholder.pop(index)
+						placeholder.insert(pivot_index, value)
+						self.frames.append(placeholder.copy())
+						framing = True
+						break
+						
 	def quick_sort(self, array):
 		if len(array) <= 1:
 			return array
 
-		pivot_index = randint(0, len(array) - 1)
-		pivot = array[pivot_index]
+		pivot = array[randint(0, len(array) - 1)]
 		self.item_log.append(pivot)
-
 		lesser, same, greater = [], [], []
 		for element in array:
 			if element == pivot:
@@ -56,7 +61,6 @@ class Sort:
 				lesser.append(element)
 			elif element > pivot:
 				greater.append(element)
-
 		return self.quick_sort(lesser) + same + self.quick_sort(greater)
 
 	def heap_sort_frames(self, array):
@@ -66,12 +70,11 @@ class Sort:
 			# Create the max heap.
 			for i in range((len(heap)//2)-1, -1, -1):
 				self.tidy_heap(heap, i, sort_array)
-
 			# Storing the max value in the sort array.
 			sort_array.insert(0, heap[0])
 			heap[0], heap[-1] = heap[-1], heap[0]
 			heap.pop(-1)
-		self.frames.append(heap + sort_array)
+		self.frames.append(sort_array)
 
 	def tidy_heap(self, heap, index, sort_array):
 		LEFT = (2 * index) + 1
